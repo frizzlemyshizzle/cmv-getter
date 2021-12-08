e4b1d4fcffe49beb17d638f1fa4471cc78a3eabc
frizzlemyshizzle <murphyj10@coventry.ac.uk>
2021-12-08 19:32:35 +0000

    Added RLTN link to constants

--- constants.py
+++ constants.py
@@ -1,2 +1,3 @@
 PROF_ENDP = 'https://api.tracker.gg/api/v2/rocket-league/standard/profile/'
 HIST_ENDP = 'https://api.tracker.gg/api/v1/rocket-league/player-history/mmr/'
+TRN = 'https://rocketleague.tracker.network/rocket-league/profile/steam/'
