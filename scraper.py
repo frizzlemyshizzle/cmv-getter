b07e022d1583855e889f7ea0742022f4481153cd
frizzlemyshizzle <murphyj10@coventry.ac.uk>
2021-12-11 20:42:15 +0000

    Prim method to get IDs

--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
 import http.client
 import reader as r
+import time
+
+start = time.time()
 
 trnIdentList = []  ## Start with empty list for rltn identifiers
 
@@ -14,18 +17,23 @@ headers = {
     }
 
 
-
-
 for profile in r.prof_linksList:
     conn.request("GET", profile, payload, headers)
     res = conn.getresponse()
-    data = res.read().split(",".encode())
-    dataList = data ## Transform data from page to list, entries seperated by ','
+    data = res.read()
+    x = str(data)
+    if 'playerId' in x: ## If 'playerId' string in data ->
+        ind = x.index('playerId') ## Get index for beginning of 'playerID'
+        ind1 = x[ind+10:ind+16] ## Iterate over start index + 10 to remove playerID string and retrieve just ID
+        trnIdentList.append(ind1) ## Add ID to list
 
-    print(dataList)
-    print('####################NEXT PLAYER ############################')
-    dataList.clear()
+        ## THIS METHOD IS REALLY SLOW, QUICKER METHODS REQ
 
+print(trnIdentList)
+print('####################################################')
+
+end = time.time()
+print('EXECUTION TIME: ', end-start)
 ##tracker_ident = (dataList[19].decode('utf-8')) ## Take 19th index of list (tracker ident)
 
 ##trnIdentList.append(tracker_ident[11:18]) ## Add Ident to trnIdent list
