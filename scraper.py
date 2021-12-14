e6e3541abb8e3f0ca769723cf2d8320dccc51ffe
frizzlemyshizzle <murphyj10@coventry.ac.uk>
2021-12-14 00:59:13 +0000

    Scraper outputs ratings to list for reader.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 import http.client
+from typing import Collection
 import reader as r
 import time
 import constants as cons
@@ -8,7 +9,7 @@ import json
 start = time.time()
 
 trnIdentList = []  ## Start with empty list for rltn identifiers
-historylinkList =[]
+historylinkList =[] ## Start with empty list for historic data links
 
 conn = http.client.HTTPSConnection("api.tracker.gg") ## Establish base of connection
 
@@ -24,15 +25,33 @@ headers = {
 for profile in r.prof_linksList: ## For each profile in list ->
     conn.request("GET", profile, payload, headers) ## Hit ENDP
     res = conn.getresponse() ## Define response
-    data = res.read()
-    x = json.loads(data)
-    if res.status == 200:
-        rltnId = x['data']['metadata']['playerId']
-        trnIdentList.append(rltnId)
-    else:
-        trnIdentList.append('#######')
-print(trnIdentList)
-historylinkList = [cons.HIST_ENDP + ident for ident in trnIdentList]
+    data = res.read() ## Read and define response
+    x = json.loads(data) ## Load response to Json
+    if res.status == 200: ## If site response = 200 (OK)
+        rltnId = x['data']['metadata']['playerId'] ## Take playerId from defined path
+        trnIdentList.append(rltnId) ## Add id to list
+        historylinkList.append(cons.HIST_ENDP + str(rltnId))
+    else: ## If response not 200 ->
+        trnIdentList.append('#######') ## Give empty ID
+        historylinkList.append(cons.HIST_ENDP + '#######')
+
+ratingList = []
+
+for link in historylinkList:
+    if link[-1:] != '#':
+        conn.request("GET", link, payload, headers) ## Hit ENDP
+        res = conn.getresponse() ## Define response
+        data = res.read() ## Read and define response
+        x = json.loads(data) ## Load response to Json
+        doublesRating = x['data']['11']
+        threesRating  = x['data']['13']
+        print(link)
+        ratingList.append(link)
+        for entry in doublesRating:
+            ratingList.append(entry['rating'])
+            ratingList.append(entry['collectDate'])
+    print(ratingList)
+        
 
 end = time.time()
 print('EXECUTION TIME: ', end-start)
